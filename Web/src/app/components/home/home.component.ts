97a716a5fbb28abba6d1e70f9a4524d06a9e3e32
Emilio Fernández <emilioutn@gmail.com>
2019-10-06 01:38:42 -0300

    cambio de palabra

--- Web/src/app/components/home/home.component.ts
+++ Web/src/app/components/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     /* Hacer lógica que verifique si se encuentra logueado. En caso de no estar 
-    logueado, redirige a pantalla de loguin */
+    logueado, redirige a pantalla de login */
     
   }
 
