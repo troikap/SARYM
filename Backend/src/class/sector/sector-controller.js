6d0c5a7481366c45c946f2c74984554f94f9f9d2
ToikaP <lucasperezgracia@gmail.com>
2019-10-11 12:50:37 -0300

    End points de tipomoneda, unindadmedida, rubro, sector

---
+++ Backend/src/class/sector/sector-controller.js
@@ -0,0 +1,151 @@
+"use strict";
+
+const SectorModelo = require("../sector/sector-model"),
+  SectorController = () => {},
+  legend = "Sector",
+  idtable = "idSector",
+  table = "sector";
+
+SectorController.getAll = (req, res, next) => {
+  SectorModelo.findAll({ raw: true }).then(projects => {
+    if (!projects || projects == 0) {
+      let locals = {
+        title: `No existen registros de ${legend}`
+      };
+      res.json(locals);
+    } else {
+      let locals = {
+        title: `${legend}`,
+        data: projects
+      };
+      res.json(locals);
+    }
+  });
+};
+
+SectorController.getOne = (req, res, next) => {
+  SectorModelo.findOne({
+    where: { [idtable]: req.params[idtable] }
+  }).then(project => {
+    if (!project || project == 0) {
+      let locals = {
+        title: "No existe el registro : " + req.params[idtable]
+      };
+      res.json(locals);
+    } else {
+      let locals = {
+        title: `${legend}`,
+        data: project.dataValues
+      };
+      res.json(locals);
+    }
+  });
+};
+
+SectorController.create = (req, res) => {
+  console.log("Estamos LLEGANDO M", req.body)
+  if (req.body[idtable]) {
+    SectorModelo.findOne({
+      where: { [idtable]: req.body[idtable] }
+    }).then(project => {
+      if (!project || project == 0) {
+        SectorModelo.create(req.body).then(result => {
+          let locals = {
+            title: `Creando ${legend}`,
+            id: result[idtable],
+            data: result
+          };
+          res.json(locals);
+        });
+      } else {
+        var check = false;
+        for (let attribute in req.body) {
+          if (
+            String(req.body[attribute]) != String(project.dataValues[attribute])
+          ) {
+            check = true;
+          }
+        }
+        if (check) {
+          SectorModelo.update(req.body, {
+            where: {
+              [idtable]: req.body[idtable]
+            }
+          }).then(result => {
+            let locals = {
+              title: `Actualizando ${legend}: ${req.body[idtable]}`
+            };
+            res.json(locals);
+          });
+        } else {
+          let locals = {
+            title: `No existe ninguna modificación de ${legend}: ${req.body[idtable]}`
+          };
+          res.json(locals);
+        }
+      }
+    });
+  } else {
+    SectorModelo.create(req.body).then(result => {
+      let locals = {
+        title: `Creando Nuevo ${legend}: ${result[idtable]}`,
+        data: result
+      };
+      res.json(locals);
+    });
+  }
+};
+
+SectorController.delete = (req, res, next) => {
+  let [idtabla] = req.params[idtabla];
+  SectorModelo.getOne([idtabla], (err, rows) => {
+    if (err) {
+      let locals = {
+        title: `Error al buscar el registro con el id: ${[idtabla]}`,
+        description: "Error de Sintaxis SQL",
+        error: err
+      };
+      res.json(locals);
+    } else {
+      let locals = {
+        title: `${leyenda}: ${[idtabla]}`,
+        data: rows
+      };
+      res.json(locals);
+    }
+  });
+};
+
+SectorController.destroy = (req, res, next) => {
+  SectorModelo.destroy({
+    where: {
+      [idtable]: req.params[idtable]
+    }
+  }).then(response => {
+    if (!response || response == 0) {
+      let locals = {
+        title: `No existe el registro de ${legend}: ` + req.params[idtable]
+      };
+      res.json(locals);
+    } else {
+      let locals = {
+        title: `${legend} Eliminado Fisicamente`
+      };
+      res.json(locals);
+    }
+  });
+};
+
+SectorController.error404 = (req, res, next) => {
+  let error = new Error(),
+    locals = {
+      title: "Error 404",
+      description: `Recurso No Encontrado`,
+      error: error
+    };
+  error.status = 404;
+  res.json(locals);
+  next();
+};
+
+module.exports = SectorController;
