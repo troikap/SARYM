511bf650ac024550e6ec933088145b1cd12eee3e
Emilio Fernández <emilioutn@gmail.com>
2019-10-14 16:18:33 -0300

    Cofiguración Librería de Jquery

--- Frontend-Web/src/app/components/abm-unidadmedida/search-unidadmedida/abm-unidadmedida.component.ts
+++ Frontend-Web/src/app/components/abm-unidadmedida/search-unidadmedida/abm-unidadmedida.component.ts
@@ -1,3 +1,4 @@
+import * as $ from 'jquery'
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // import { UnidadMedida } from '../../model/unidad-medida/unidad-medida.model'; //Da error
--- Frontend-Web/src/app/components/abm-usuario/search-usuario/abm-usuario.component.ts
+++ Frontend-Web/src/app/components/abm-usuario/search-usuario/abm-usuario.component.ts
@@ -1,3 +1,4 @@
+import * as $ from 'jquery'
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
