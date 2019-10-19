eab09f49a04dfa1b5aba0d20e2e741bead30e579
Rodrigo Cámara <rodrigocamararobles@gmail.com>
2019-10-19 19:39:03 -0300

    search Mesas andando

--- Frontend-Web/src/app/components/abm-mesa/search-mesa/abm-mesa.component.ts
+++ Frontend-Web/src/app/components/abm-mesa/search-mesa/abm-mesa.component.ts
@@ -1,4 +1,7 @@
+import * as $ from 'jquery'
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MesaService } from 'src/app/services/mesa/mesa.service';
 
 @Component({
   selector: 'app-abm-mesa',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbmMesaComponent implements OnInit {
 
-  constructor() { }
+  listaMesas: any = [];
+
+  constructor(
+    private mesaService: MesaService,
+    private router: Router,
+  ) { }
+
 
   ngOnInit() {
+    this.getAllElements();
+    this.cargarOnFocus();
   }
 
+  cargarOnFocus() {
+    $("#botonBuscar").focus();
+  }
+
+  buscarElemento(termino: string) {
+    console.log(termino);
+    if (termino.trim() !== "") {
+      this.mesaService.getMesaByAll(termino)
+        .subscribe((data: any) => { // Llamo a un Observer
+          console.log(data);
+          if (data != null) {
+            console.log("RESULT ----------------->", data);
+            this.listaMesas = data;
+          }
+          else {
+            this.listaMesas = [];
+          }
+        });
+    }
+    else {
+      this.getAllElements();
+    }
+  }
+
+  getAllElements() {
+    this.mesaService.getMesas()
+      .then((res: any) => {
+        console.log("TRAIGO: ", res);
+        this.listaMesas = res.data;
+      })
+  }
+
+  crudElemento(idElemento: number, accion: string) {
+    console.log("idElemento: ", idElemento);
+    console.log("accion: ", accion);
+    this.router.navigate([`/mesa_crud/${idElemento}/${accion}`]);
+  }
 }
