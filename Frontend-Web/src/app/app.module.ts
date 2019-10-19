c6f832384d03ce68024fbd1e51e6a0be71380f9e
Emilio Fernández <emilioutn@gmail.com>
2019-10-19 19:20:31 -0300

    Menu Promocion

--- Frontend-Web/src/app/app.module.ts
+++ Frontend-Web/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { AbmMesaComponent } from './components/abm-mesa/abm-mesa.component';
 import { AbmRubroComponent } from './components/abm-rubro/search-rubro/abm-rubro.component';
 import { AbmSectorComponent } from './components/abm-sector/abm-sector.component';
 import { GestionarProductoComponent } from './components/gestionar-producto/search-gestionar-producto/gestionar-producto.component';
-import { GestionarMenupromocionComponent } from './components/gestionar-menupromocion/gestionar-menupromocion.component';
 import { GenerarReporteComponent } from './components/generar-reporte/generar-reporte.component';
 import { AbmUnidadmedidaCreateComponent } from './components/abm-unidadmedida/crud-unidadmedida/abm-unidadmedida-crud.component';
 import { CrudRubroComponent } from './components/abm-rubro/crud-rubro/crud-rubro.component';
@@ -27,6 +26,9 @@ import { CrudTipomonedaComponent } from './components/abm-tipomoneda/crud-tipomo
 import { CrudGestionarProductoComponent } from './components/gestionar-producto/crud-gestionar-producto/crud-gestionar-producto.component';
 import { ConsultaGestionarProductoComponent } from './components/gestionar-producto/consulta-gestionar-producto/consulta-gestionar-producto.component';
 import { NoimagePipe } from './pipes/noimage-producto.pipe';
+import { CrudGestionarMenupromocionComponent } from './components/gestionar-menupromocion/crud-gestionar-menupromocion/crud-gestionar-menupromocion.component';
+import { ConsultaGestionarMenupromocionComponent } from './components/gestionar-menupromocion/consulta-gestionar-menupromocion/consulta-gestionar-menupromocion.component';
+import { GestionarMenupromocionComponent } from './components/gestionar-menupromocion/search-gestionar-menupromocion/gestionar-menupromocion.component';
 
 
 @NgModule({
@@ -51,7 +53,9 @@ import { NoimagePipe } from './pipes/noimage-producto.pipe';
     CrudTipomonedaComponent,
     CrudGestionarProductoComponent,
     ConsultaGestionarProductoComponent,
-    NoimagePipe
+    NoimagePipe,
+    CrudGestionarMenupromocionComponent,
+    ConsultaGestionarMenupromocionComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend-Web/src/app/components/gestionar-menupromocion/consulta-gestionar-menupromocion/consulta-gestionar-menupromocion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-gestionar-menupromocion',
+  templateUrl: './consulta-gestionar-menupromocion.component.html',
+  styleUrls: ['./consulta-gestionar-menupromocion.component.scss']
+})
+export class ConsultaGestionarMenupromocionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
