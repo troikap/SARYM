afb0f2b82018d732c2d6cdf09336fb6fbdca8ae5
Emilio Fernández <emilioutn@gmail.com>
2019-11-07 16:28:15 -0300

    cambios

--- Frontend-Mobile/src/app/app-routing.module.ts
+++ Frontend-Mobile/src/app/app-routing.module.ts
@@ -28,6 +28,8 @@ const routes: Routes = [
 // catalogo
   { path: 'catalogo', loadChildren: './catalogo/catalogo.module#CatalogoPageModule' },
   { path: 'modal-detalle-catalogo', loadChildren: './modal/modal-detalle-catalogo/modal-detalle-catalogo.module#ModalDetalleCatalogoPageModule' },
+ 
+ // reserva
   { path: 'search-gestionar-reserva', loadChildren: './pages/gestionar-reserva/search-gestionar-reserva/search-gestionar-reserva.module#SearchGestionarReservaPageModule' },
   { path: 'crud-gestionar-reserva/:id/:accion', loadChildren: './pages/gestionar-reserva/crud-gestionar-reserva/crud-gestionar-reserva.module#CrudGestionarReservaPageModule' },
   { path: 'search-gestionar-reserva', loadChildren: './pages/gestionar-reserva/search-gestionar-reserva/search-gestionar-reserva.module#SearchGestionarReservaPageModule' },
