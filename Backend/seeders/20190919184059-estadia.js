3c04f4dac66c54a439add3014d90b07c27283cb8
ToikaP <lucasperezgracia@gmail.com>
2019-11-08 18:53:12 -0300

    nada

--- Backend/seeders/20190919184059-estadia.js
+++ Backend/seeders/20190919184059-estadia.js
@@ -6,27 +6,11 @@ var currentDate = fechaArgentina.getFechaArgentina();
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('estadia', [
-            {
-                "idEstadia": 1,
-                "idReserva": 1,
-                "idMozoEstadia": 1,
-                "cantPersonas": 8,
-                "fechaYHoraInicioEstadia": currentDate,
-                "fechaYHoraFinEstadia": null,
-                "tokenEstadia": "TOKEN123456",
-                "createdAt": currentDate,
-                "updatedAt": currentDate
+            { 
+                "idEstadia": 1, "idReserva": 1, "idMozoEstadia": 1, "cantPersonas": 8, "fechaYHoraInicioEstadia": currentDate, "fechaYHoraFinEstadia": null, "tokenEstadia": "TOKEN123456", "createdAt": currentDate, "updatedAt": currentDate
             },
-            {
-                "idEstadia": 2,
-                "idReserva": 2,
-                "idMozoEstadia": 1,
-                "cantPersonas": 9,
-                "fechaYHoraInicioEstadia": currentDate,
-                "fechaYHoraFinEstadia": null,
-                "tokenEstadia": "TOKEN123456",
-                "createdAt": currentDate,
-                "updatedAt": currentDate
+            { 
+                "idEstadia": 2, "idReserva": null, "idMozoEstadia": 1, "cantPersonas": 9, "fechaYHoraInicioEstadia": currentDate, "fechaYHoraFinEstadia": null, "tokenEstadia": "TOKEN123456", "createdAt": currentDate, "updatedAt": currentDate
             },
     ], {});
     },
--- Backend/seeders/20190919184557-comensal.js
+++ Backend/seeders/20190919184557-comensal.js
@@ -10,10 +10,10 @@ module.exports = {
                 "idComensal": 1, "idUsuario": 1, "idReserva": 1, "idEstadia": 1, "aliasComensal": "Pepito Honguito", "edadComensal": 45, "createdAt": currentDate, "updatedAt": currentDate
             },
             { 
-                "idComensal": 2, "idUsuario": null, "idReserva": 2, "idEstadia": 2, "aliasComensal": "Otro Pepito", "edadComensal": 20, "createdAt": currentDate, "updatedAt": currentDate
+                "idComensal": 2, "idUsuario": null, "idReserva": 1, "idEstadia": 1, "aliasComensal": "Otro Pepito", "edadComensal": 20, "createdAt": currentDate, "updatedAt": currentDate
             },
             { 
-                "idComensal": 3, "idUsuario": 2, "idReserva": null, "idEstadia": 1, "aliasComensal": "Tercer Pepo", "edadComensal": 50, "createdAt": currentDate, "updatedAt": currentDate
+                "idComensal": 3, "idUsuario": 2, "idReserva": 2, "idEstadia": null, "aliasComensal": "Tercer Pepo", "edadComensal": 50, "createdAt": currentDate, "updatedAt": currentDate
             },
             { 
                 "idComensal": 4, "idUsuario": 17, "idReserva": 3, "idEstadia": null, "aliasComensal": "Pepito Honguito", "edadComensal": 45, "createdAt": currentDate, "updatedAt": currentDate
