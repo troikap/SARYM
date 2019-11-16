fbaee9a372688b0c886ae64df606d0bbfc7840d3
Rodrigo Cámara <rodrigocamararobles@gmail.com>
2019-11-16 15:30:09 -0300

    Validaciones fechas para generar reporte hechas, servicio de generar reporte cabeceras hecha

--- Frontend-Web/src/app/components/generar-reporte/generar-reporte.component.ts
+++ Frontend-Web/src/app/components/generar-reporte/generar-reporte.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as jsPDF from 'jspdf';
 import * as html2canvas from 'html2canvas';
+import { FormGroup, Validators, FormControl } from "@angular/forms";
+import { GenerarReporteService } from '../../services/generar-reporte/generar-reporte.service';
 
 @Component({
   selector: 'app-generar-reporte',
@@ -9,14 +11,28 @@ import * as html2canvas from 'html2canvas';
 })
 export class GenerarReporteComponent implements OnInit {
   
+  form: FormGroup;
   public mostrar = false;
   public fechaActual;
+  public fechaDesde: Date;
+  public fechaHasta: Date;
 
-  constructor() {
+
+  constructor( private reporteService: GenerarReporteService ) {
     this.fechaActual = new Date().toJSON().split('T')[0];
+    this.form = new FormGroup({
+      fechaDesde: new FormControl("", Validators.required),  
+      fechaHasta: new FormControl("", Validators.required),  
+    });
+
+    this.form.get("fechaDesde").setValidators(Validators.required);
+    this.form.get("fechaDesde").updateValueAndValidity();
+    this.form.get("fechaHasta").setValidators(Validators.required);
+    this.form.get("fechaHasta").updateValueAndValidity();
+    this.validadorFecha();
   }
 
-  ngOnInit() {
+  ngOnInit() {    
   }
   
 
@@ -29,13 +45,13 @@ export class GenerarReporteComponent implements OnInit {
     const length = ids.length;
     for (let i = 2; i < length; i++) {
       const chart = document.getElementById(ids[i].id);
-      await html2canvas(chart, {       
+      await html2canvas(chart, {
         allowTaint: true,
         useCORS: false,
         width: 850,
         height: 1050,
         // Calidad del PDF
-        scale: 2, 
+        scale: 2,
       }).then(function (canvas) {
         doc.addImage(canvas.toDataURL('image/png'), 'JPEG', 10, 20, 190, 250);
         if (i < (length - 1)) {
@@ -47,8 +63,39 @@ export class GenerarReporteComponent implements OnInit {
     doc.save('Reporte_Sarym_' + fecha + '.pdf');
   }
 
-  mostrarReportes(){
-    this.mostrar = true;   
+  guardar() {
+  }
+
+  mostrarReportes() {
+    this.fechaDesde = this.form.value["fechaDesde"];
+    this.fechaHasta = this.form.value["fechaHasta"];
+    console.log("fechaDesde:", this.fechaDesde);
+    console.log("fechaHasta:", this.fechaHasta);
+
+    this.validadorFecha();
+    // LLAMAR AL SERVICIO DE GER REPORTES
+    //this.reporteService.getReportes();
+    this.mostrar = true;
+  }
+
+  validadorFecha() {
+    this.form.get("fechaDesde").valueChanges.subscribe(resp => {
+      if (resp < this.form.value.fechaHasta) {
+        this.form.controls.fechaHasta.setErrors(null);
+      } else {
+        console.log("MAL DESDE FECHA DESDE");
+        this.form.controls.fechaHasta.setErrors({ not_equal: true });
+      }
+    });
+    this.form.get("fechaHasta").valueChanges.subscribe(resp => {
+      if (resp > this.form.value.fechaDesde) {
+        console.log("OK DESDE FECHA HASTA");
+        this.form.controls.fechaHasta.setErrors(null);
+      } else {
+        console.log("MAL DESDE FECHA HASTA");
+        this.form.controls.fechaHasta.setErrors({ not_equal: true });
+      }
+    });
   }
 
   //http://blog.nubecolectiva.com/generar-pdf-con-angular-js-5/
