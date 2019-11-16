aeffd3d86d3ba68addfc57e45026b901ae509ff8
Emilio Fernández <emilioutn@gmail.com>
2019-11-16 15:30:43 -0300

    Modulo Backup

--- Frontend-Web/src/app/components/backup-module/backup-module.component.ts
+++ Frontend-Web/src/app/components/backup-module/backup-module.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackupService } from 'src/app/services/backup/backup.service';
 
 @Component({
   selector: 'app-backup-module',
@@ -6,18 +8,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backup-module.component.scss']
 })
 export class BackupModuleComponent implements OnInit {
+  public uploadedFiles = null;
+  public backupGenerado = true;
+  public backupCargado = true;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public backupService: BackupService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   generarBackup() {
     console.log("Generar Backup");
+    
+    this.backupGenerado = false;
+
+    this.backupService.generarBackup().then((res: any) => {
+        
+      console.log("res: ", res);
+      
+      ($ as any).confirm({
+        title: "Éxito",
+        content: "Su archivo de Backup ha sido generado con éxito",
+        type: "green",
+        typeAnimated: true,
+        theme: "material",
+        buttons: {
+          aceptar: {
+            text: "Aceptar",
+            btnClass: "btn-green",
+            action: function() {
+              this.backupGenerado = true;
+            }
+          }
+        }
+      });
+    });
   }
 
-  getBackup() {
-    console.log("Recuperar datos desde Backup");
+  cargarBackup() {
+    console.log("cargarBackup");
+
+    if (this.uploadedFiles != null) {
+
+      this.backupCargado = false;
+
+      const archivo = this.uploadedFiles;
+      const formData = new FormData();
+      formData.append("archivo", archivo);
+      this.backupService.cargarBackup(formData).then((res: any) => {
+        
+        console.log("res: ", res);
+        
+        ($ as any).confirm({
+          title: "Éxito",
+          content: "Su archivo de Backup ha sido cargada con éxito",
+          type: "green",
+          typeAnimated: true,
+          theme: "material",
+          buttons: {
+            aceptar: {
+              text: "Aceptar",
+              btnClass: "btn-green",
+              action: function() {
+                this.backupCargado = true;
+              }
+            }
+          }
+        });
+      });
+    } else {
+      ($ as any).confirm({
+        title: "Error",
+        content: "Seleccione imagen..",
+        type: "red",
+        typeAnimated: true,
+        theme: "material",
+        buttons: {
+          aceptar: {
+            text: "Aceptar",
+            btnClass: "btn-red",
+            action: function() {}
+          }
+        }
+      });
+    }
+  }
+
+  onFileChange(e) {
+    if (e.target.files && e.target.files[0]) {
+      this.uploadedFiles = e.target.files[0];
+    }
   }
 
 }
