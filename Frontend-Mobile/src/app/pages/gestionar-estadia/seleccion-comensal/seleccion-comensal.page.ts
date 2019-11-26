f9c69a148a55d9a4033a17bbf69674e5bb68fadf
Emilio Fernández <emilioutn@gmail.com>
2019-11-26 18:32:12 -0300

    Cambios Emilio

--- Frontend-Mobile/src/app/pages/gestionar-estadia/seleccion-comensal/seleccion-comensal.page.ts
+++ Frontend-Mobile/src/app/pages/gestionar-estadia/seleccion-comensal/seleccion-comensal.page.ts
@@ -60,7 +60,7 @@ export class SeleccionComensalPage implements OnInit {
   
     ionViewWillEnter(){
       this.storage.getComensales().then((respuesta) => {
-        console.log("Trayendo Comensales Reserva", respuesta)
+        console.log("Trayendo Comensales Estadia", respuesta)
         if (respuesta != null) {
           respuesta.forEach(element => {
             if(element.idEstadia == this.idEstadia){
@@ -84,19 +84,19 @@ export class SeleccionComensalPage implements OnInit {
   
     limpiarComensalStorage(){
       this.storage.validarComensal().then((respuesta) => {
-        console.log("Limpiando Comensales Reserva", respuesta)
+        console.log("Limpiando Comensales Estadia", respuesta)
         if(respuesta) {
           respuesta.forEach(element => {
             if(element.vencida) {
               let data: {} = {idEstadia: element.idEstadia,
                 idEstadoEstadia: 2,
-                descripcionReservaEstado: `Por Vencimiento, eliminado desde Comensal ${element.idComensal}.`}
+                descripcionEstadiaEstado: `Por Vencimiento, eliminado desde Comensal ${element.idComensal}.`}
               this.estadiaService.cambiarEstado(data)
               .then( resp => {
                 if(resp.tipo == 1){
-                  this.toastService.toastError( `Reserva N° ${element.idEstadia} Anulada por vencimiento.`,3000,'bottom')
+                  this.toastService.toastError( `Estadia N° ${element.idEstadia} Anulada por vencimiento.`,3000,'bottom')
                 } else {
-                  this.toastService.toastWarning( `Reserva N° ${element.idEstadia} Anulada por vencimiento.`,3000,'bottom')
+                  this.toastService.toastWarning( `Estadia N° ${element.idEstadia} Anulada por vencimiento.`,3000,'bottom')
                 }
               })
             }
@@ -108,7 +108,7 @@ export class SeleccionComensalPage implements OnInit {
     traerComensalEstadiaStorage(){
       if(!this.modificarComensal){
         this.storage.getComensales().then((respuesta) => {
-          console.log("Trayendo Comensales Reserva", respuesta)
+          console.log("Trayendo Comensales Estadia", respuesta)
           if (respuesta != null ){
             respuesta.forEach(element => {
               if(element.idEstadia == this.idEstadia){
@@ -182,8 +182,87 @@ export class SeleccionComensalPage implements OnInit {
       await alert.present();
     } 
   
-    eliminarComensal( item ) {
-      console.log("ELIMINADN COMENSAL", item)
+    async eliminarComensal( item ) {
+      const alert = await this.alertController.create({
+        header: 'Confirmar',
+        message: `¿Desea Eliminar el comensal seleccionado?`,
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: ( resp ) => {
+              // no hacer nada
+            }
+          }, {
+            text: 'Eliminar Comensal',
+            handler: () => {
+              let pathComensal = { idEstadia: this.idEstadia,detalle: [ { idComensal: item['idComensal'], baja: true}]};
+              this.estadiaService.setComensalesEstadia(pathComensal)
+              .then( respuesta => {
+                if (respuesta.tipo == 1){
+                  this.toastService.toastSuccess('Comensal eliminado correctamente.', 1500)
+                  this.traerEstadia();
+                } else {
+                  this.ConfirmarEliminarComensalAsociado('Confirmar Eliminado', 'El Comensal posee pedidos asociados. ¿Desea eliminar el Comensal con todos sus Pedidos?', pathComensal, item['idComensal']);
+                }
+              }).catch( error => {
+                console.log("ERROR ", error)
+              });
+            }
+          }
+        ],
+        cssClass: 'alertWarning',
+      });
+      await alert.present();
+    }
+
+    async ConfirmarEliminarComensalAsociado(pTitulo: string, pMensaje: string, pathComensal, idComensal: number) {
+      const alert = await this.alertController.create({
+        header: pTitulo,
+        message: pMensaje,
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: (blah) => {
+              console.log('Cancelado');
+            }
+          }, {
+            text: 'Eliminar',
+            handler: ( info ) => {
+              if (this.verificarEliminarComensalEstadia(idComensal)) {
+                console.log("Obligar eliminacion de comensal con pedidos asociados")
+                this.estadiaService.setComensalesEstadia(pathComensal, true)
+                .then( respuesta => {
+                  if ( respuesta.tipo == 1 ){
+                    this.toastService.toastSuccess(`Comensal eliminado Correctamente con todos sus Pedidos asociados.`, 2500)
+                    this.traerEstadia();
+                  }
+                })
+              }
+              else {
+                this.toastService.toastError(`No se ha podido eliminar el Comensal. El mismo posee Pedidos Finalizados o Pendientes de Pago.`, 3000)
+              }
+            }
+          }
+        ],
+        cssClass: 'alertWarning',
+      })
+      await alert.present();
+    }
+
+    verificarEliminarComensalEstadia (idComensal: number): boolean { 
+      //Si al menos un pedido se encuentra en ciertos estados, NO permitir eliminar Comensal
+      for (let item of this.estadia.pedidos) {
+        if (idComensal == item.idComensal) {
+          let estadoPedido = item['pedidoestados'][0].idPedidoEstado;
+          if (estadoPedido == 5 || estadoPedido == 6 || estadoPedido == 7 ) { // Que no permite si estado es: Finalizado, Finalizado Sin Pagar, Pendiente de Pago
+            return false;
+          }
+        }
+      }
+      return true;
     }
   
     crearComensal() {
