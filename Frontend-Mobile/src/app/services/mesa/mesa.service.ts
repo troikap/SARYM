8f7a54963fdfab994bc263dfd9b7f24019ebfcbd
Emilio Fernández <emilioutn@gmail.com>
2019-11-30 06:07:14 -0300

    Validación Home - Cambio de estado automatico de Reservas y Mesas

--- Frontend-Mobile/src/app/services/mesa/mesa.service.ts
+++ Frontend-Mobile/src/app/services/mesa/mesa.service.ts
@@ -10,6 +10,7 @@ import { Mesa } from './mesa.model';
 export class MesaService {
   url = environment.urlNgrok || environment.url;
   dir = '/mesa';
+  dirCambiarEstado = '/cambiarEstado';
 
   tokenEnviroment = environment.token;
 
@@ -20,19 +21,32 @@ export class MesaService {
   getMesas(): Promise<Mesa[]> {
     console.log("ENTRANDO A GET MESA")
     let headers: HttpHeaders = new HttpHeaders();
-     headers = headers.append('token', this.tokenEnviroment);
+    headers = headers.append('token', this.tokenEnviroment);
     return this.http
-      .get(`${this.url}${this.dir}`, {headers})
-      .toPromise()
-      .then(response => {
-        return response as Mesa[];
-      })
-      .catch( err => {
-        console.log("ERRROR ", err)
-        return err
-      });
+    .get(`${this.url}${this.dir}`, {headers})
+    .toPromise()
+    .then(response => {
+      return response as Mesa[];
+    })
+    .catch( err => {
+      console.log("ERRROR ", err)
+      return err
+    });
   }
 
+  cambiarEstado( datas: any ): Promise<any> {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('token', this.tokenEnviroment);
+    console.log("DATOS A ENVIAR :",datas)
+    return this.http
+    .put(`${this.url}${this.dir}${this.dirCambiarEstado}`, datas, {headers})
+    .toPromise()
+    .then(response => {
+      console.log("Servicio cambiarEstadoMesa()", response);
+      return response;
+    })
+    .catch(  );
+  }
 }
 
 
