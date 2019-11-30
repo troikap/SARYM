88d16176ce45cc1d2e90d7a1e8a697a6a25b0be3
ToikaP <lucasperezgracia@gmail.com>
2019-11-30 04:20:33 -0300

    delete two archivos wacho

--- Frontend-Mobile/src/app/pages/mozo/consultar-salon/consultar-salon.page.ts
+++ Frontend-Mobile/src/app/pages/mozo/consultar-salon/consultar-salon.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MesaService } from "src/app/services/mesa/mesa.service";
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-consultar-salon',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultarSalonPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private mesaService: MesaService,
+    private navController: NavController
+  ) {
+    console.log("Constructor Consulta SAlon")
+    this.traerMesas();
+   }
 
   ngOnInit() {
   }
 
+  traerMesas() {
+    this.mesaService.getMesas().then( mesas => {
+      console.log("MESAS TRAIDAS ", mesas)
+    })
+  }
 }
